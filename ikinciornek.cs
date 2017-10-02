f6e598d5f1d25b08383f7c128e3a8cd3ade6ba5a
gokmenmstf <32059456+gokmenmstf@users.noreply.github.com>
2017-10-02 19:52:45 +0300

    İkinciÖrnek

---
+++ ikinciornek.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, toplam;
+            Console.Write("İlk Değeri Giriniz=");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.Write("İkinci Değeri Giriniz=");
+            b = Int32.Parse(Console.ReadLine());
+
+            toplam = a + b;
+
+            Console.WriteLine();
+
+            Console.WriteLine("Sayıların Toplamı="+toplam);
+            Console.ReadKey();
+            
+
+            
+        }
+    }
+}
