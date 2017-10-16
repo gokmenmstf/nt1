0e84f953448a9b876d81e30acf9f2f4e1114955a
Gökmen <gokmenmstf42@gmail.com>
2017-10-16 19:59:08 +0300

    bisiklet örnek

---
+++ bisiklet örnek.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    using System;
+
+    namespace BisikletUygulama
+    {
+
+        class Bisiklet
+        {
+            int hiz, vites, vitesSayisi, tekerCapi; // Default private
+            public Bisiklet()  // Kurucu metod
+            {
+                vitesSayisi = 6;
+            }
+            public Bisiklet(int v) // kurucu metod aşırı yükleme 
+            {
+                vitesSayisi = v;
+            }
+            public void Hizlan(int artis)
+            {
+                hiz += artis;
+            }
+            public void FrenYap(int azalis)
+            {
+                hiz -= azalis;
+            }
+            public void VistesDegistir(int yeniDeger)
+            {
+                vites = yeniDeger;
+            }
+            public void BilgileriYaz()
+            {
+                Console.WriteLine("Şu anki hiziniz : {0}", hiz);
+                Console.WriteLine("Şu anki vitesiniz : {0}", vites);
+                Console.WriteLine("En yuksek vites  {0}:", vitesSayisi);
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Bisiklet b1 = new Bisiklet();
+                Bisiklet b2 = new Bisiklet();
+                Bisiklet b3 = new Bisiklet();
+                Bisiklet b4 = new Bisiklet();
+                Bisiklet b5 = new Bisiklet();
+
+                b1.Hizlan(10);
+                b1.VistesDegistir(3);
+                b1.BilgileriYaz();
+                b1.FrenYap(5);
+                b1.VistesDegistir(2);
+                b1.BilgileriYaz();
+
+                Console.WriteLine("toplam bisiklet sayisi :" + Convert.ToString(Bisiklet.bsayisi));
+                
+
+            }
+        }
+    }
+}
+    
